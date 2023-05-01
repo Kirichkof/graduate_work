fc2ab15087980082e4a5801d3b757ccf986f2f83
Kirichkof <d-kirichkov@mail.ru>
2023-05-01 10:47:04 +0300

    сделал электронную очередь

---
+++ stage_2.1.py
@@ -0,0 +1,20 @@
+def main():
+    eq = []
+    ver = '2.1'
+    while True:
+        eq_print(eq, ver)
+        data = input('Введите фамилию или пустая строка для выхода: ')
+        if data == '':
+            print('Всего вам доброго. До свидания.')
+            break
+        eq.append(data)
+
+def eq_print(eq: list, version: str) -> None:
+    print(f'Электронная очередь. Версия {version}')
+    print('*' * 25)
+    for client in eq:
+        print(f'|{client:^15s}|')
+    print('*' * 25)
+
+
+main()
