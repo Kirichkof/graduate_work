1731cd18ff00503113f1b89f6c7f2e2eeb1bbf34
Kirichkof <d-kirichkov@mail.ru>
2023-05-25 17:19:22 +0300

    commit

--- e_queue.py
+++ e_queue.py
@@ -1,14 +1,29 @@
+from datetime import datetime, timedelta
+from random import randint, choice
+from pathlib import Path
 
 
-def is_code_valid(code: str, codes: str)-> bool:
-    return code.upper() in codes
+def is_code_valid(code: str, spec: dict)-> bool:
+    return code.upper() in spec['codes_available']
 
 
-def eq_add_client(eq: dict, idc: int, surname: str, code: str)-> list:
+def eq_add_client(eq: dict, idc: int, surname: str, code: str, spec: dict):
+    enter = datetime.now()
+    timing = timedelta(seconds = randint(5, 10))
+    win = choice(spec['win_codes'][code.upper()])
+    idc_prev = spec['win_status'][win]
+    if idc_prev is None:
+        stop = enter + timing
+    else:
+        stop = eq[idc_prev]['stop'] + timing
+    spec['win_status'][win] = idc
     eq[idc] = {'surname': surname, 'code': code.upper(),
-               'date': datetime.now(),
-               'timing': timedelta(seconds = randint(5, 10))}
-    return eq
+               'date': enter,
+               'timing': timing,
+               'win': win,
+               'stop': stop
+               }
+    return eq, spec
 
 
 def idc_str(idc: int) -> str:
@@ -20,11 +35,13 @@ def table_row_str(idc: int, client: dict, spec: dict) -> str:
     surname = client['surname'] if len(client['surname']) <= 10 else client['surname'][:10] + '...'
     sep = spec['output']['sep']
     timing = str(client['timing'])[2:]
+    win = client['win']
     result = f"{sep}{idc_str(idc):^{spec['output']['idc_width']}s}{sep}" + \
              f"{surname:^{spec['output']['surname_width']}s}{sep}" + \
              f"{client['code']:^{spec['output']['code_width']}s}{sep}" + \
              f"{date:^{spec['output']['date_width']}s}{sep}" + \
-             f"{timing:^{spec['output']['timing_width']}s}{sep}"
+             f"{timing:^{spec['output']['timing_width']}s}{sep}" + \
+             f"{win:^{spec['output']['win_width']}s}{sep}"
     return result
 
 
@@ -36,8 +53,10 @@ def eq_print(eq: dict, version: str, spec: dict) -> None:
     code_width = spec['output']['code_width']
     date_width = spec['output']['date_width']
     timing_width = spec['output']['timing_width']
-
-    total_width = idc_width + surname_width + code_width + date_width + timing_width + 6
+    win_width = spec['output']['win_width']
+    
+    total_width = idc_width + surname_width + code_width + date_width + \
+                  timing_width + win_width + 7
     print('-' * total_width)
     sep = spec['output']['sep']
 
@@ -45,10 +64,69 @@ def eq_print(eq: dict, version: str, spec: dict) -> None:
              f"{'Фамилия':^{surname_width}s}{sep}" + \
              f"{'Код':^{code_width}s}{sep}" + \
              f"{'Время':^{date_width}s}{sep}" + \
-             f"{'Длит.':^{timing_width}s}{sep}"
+             f"{'Длит.':^{timing_width}s}{sep}" + \
+             f"{'Окно':^{win_width}s}{sep}"
     print(header)
     print('-' * total_width)
 
     for idc, client, in eq.items():
         print(table_row_str(idc, client, spec))
     print('-' * total_width)
+
+
+def man_cases(number: int) -> str:
+    remain = number % 100
+    if remain >= 20:
+        remain %= 10
+    if 2 <= remain <= 4:
+        return 'a'
+    return ''
+
+
+def eq_print_footer(eq: dict, spec: dict) -> None:
+    lenght = len(eq)
+    print(f'Перед вами {lenght} человек{man_cases(lenght)}')
+    if lenght != 0:
+        mean = sum([client['timing'] for client in eq.values()], start= timedelta(0,0,0,0,0,0))
+        mean = str(mean/lenght)[2:7]
+        print(f'Среднее время обслуживания {mean}')
+
+def codes_available(spec: dict) -> str:
+    return "".join({code for codes in spec['windows'].values() for code in codes})
+
+
+def windows_by_code(spec: dict) -> dict:
+    result = {}
+    all_codes = codes_available(spec)
+    for code in all_codes:
+        result[code] = []
+        for window, codes in spec['windows'].items():
+            if code in codes:
+                result[code].append(window)
+    return result
+
+
+def eq_clear(eq: dict, spec: dict):
+    now = datetime.now()
+    idc_pop = [idc for idc, client in eq.items() if client['stop'] < now]
+    for idc in idc_pop:
+        client_win = eq[idc]['win']
+        if spec['win_status'][client_win] == idc:
+            spec['win_status'][client_win] = None
+        eq.pop(idc)
+    return eq, spec
+
+
+def eq_write_logfile(eq: dict, spec: dict) -> None:
+    if len(eq) == 0: return
+    dir_log = Path(__file__).parent / spec['files']['log_dir']
+    if not dir_log.exists():
+        dir_log.mkdir()
+    postfix = datetime.now().strftime("%Y-%m-%d_=_%H-%M-%S")
+    file_name = spec['files']['logfile_template'] + postfix + '.txt'
+    file_path = dir_log / file_name
+    with open(file_path, mode = 'wt', encoding='utf-8') as file:
+        for idc, client in eq.items():
+            record = f"{idc}. {client['surname']}, {client['code']}, {client['date']}, {client['timing']}, {client['win']}\n"
+            file.write(record)
+    return file_name
--- main.py
+++ main.py
@@ -1,1 +1,56 @@
-print('hello world')
+from datetime import datetime, timedelta
+from random import choice
+import e_queue as q
+import e_client as c
+
+def main():
+    windows = {'win1': 'M', 
+               'win2': 'NS', 
+               'win3': 'P', 
+               'win4': 'N', 
+               'win5': 'MS'}
+    output = {
+        'idc_width': 5,
+        'surname_width': 15,
+        'code_width': 3,
+        'date_width': 20,
+        'timing_width': 7,
+        'win_width': 6,
+        'stop_width': 20,
+        'sep': '|'
+    }
+    files = {'log_dir': 'queue_log', 'logfile_template': 'base_log'}
+    spec = {'output': output, 'windows': windows, 'files': files}
+
+    idc = 1
+    eq = {}
+    ver = '4.2'
+    spec['codes_available'] = q.codes_available(spec)
+    spec['win_codes'] = q.windows_by_code(spec)
+    spec['win_status'] = {win: None for win in spec['windows'].keys()} 
+
+    try:
+        while True:
+            eq, spec = q.eq_clear(eq, spec)
+            q.eq_print(eq, ver, spec)
+            q.eq_print_footer(eq, spec)
+            surname = c.get_surname()
+            if surname == '':
+                raise KeyboardInterrupt
+            while True:
+                code = c.get_codes(spec)
+                if q.is_code_valid(code, spec):
+                    break
+                else:
+                    print('Неправильный код операции.')
+            eq, spec = q.eq_clear(eq, spec)
+            eq, spec = q.eq_add_client(eq, idc, surname, code, spec)
+            idc += 1
+    except KeyboardInterrupt:
+        filename = q.eq_write_logfile(eq, spec)
+        print(f'Файл записан в {filename}')
+        print("Всего вам доброго. До свидания")
+
+
+if __name__ == "__main__":
+    main()
